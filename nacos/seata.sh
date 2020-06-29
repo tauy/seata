ddb146e908a97cd7c24b5dd9d43bb4928836e9fd
tau <ying_961106@163.com>
2020-06-29 15:08:53 +0800

    first

---
+++ nacos/seata.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+base_path=/data/docker/seata
+container_name=seata
+port=8091
+seata_ip=192.168.60.16
+
+./nacos-config.sh
+
+mkdir -p $base_path/conf
+
+cp registry.conf $base_path/conf/.
+
+docker run --name seata-server -itd --restart=always \
+  --name $container_name \
+  -p $port:8091 \
+  -e SEATA_CONFIG_NAME=file:/root/seata-config/registry \
+  -e SEATA_IP=$seata_ip \
+  -v $base_path/conf:/root/seata-config \
+  seataio/seata-server
